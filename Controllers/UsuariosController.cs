20a8d3108c0f30598342c611d805704f7d3d1ece
Lucas Simões <lucasbritosimoes@hotmail.com>
2025-08-24 15:05:07 -0300

    Usuarios CRUD

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -1,6 +1,8 @@
 ﻿using ControleDeContatos.Models;
 using ControleDeContatos.Repositorio;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ControleDeContatos.Controllers
 {
@@ -25,6 +27,41 @@ namespace ControleDeContatos.Controllers
             return View();
         }
 
+        public IActionResult Editar(int id)
+        {
+            UsuariosModel usuarios = _usuarioRepositorio.BuscarID(id);
+            return View(usuarios);
+        }
+
+        //Métodos
+        public IActionResult ApagarConfirmacao(int id)
+        {
+            UsuariosModel usuarios = _usuarioRepositorio.BuscarID(id);
+            return View(usuarios); 
+        }
+
+        public IActionResult Apagar(int id)
+        {
+            try
+            {
+                bool apagado = _usuarioRepositorio.Apagar(id);
+                if (apagado)
+                {
+                    TempData["MensagemSucesso"] = "Usuário excluído com sucesso"; 
+                }else
+                {
+                    TempData["MensagemErro"] = "Ocorreu um erro na exclusão do usuário";
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                TempData["MensagemErro"] = "Ops, ocorreu o seguinte erro: " + ex.Message;
+                return RedirectToAction("Index");   
+            }
+        }
+
         [HttpPost]
         public IActionResult Criar(UsuariosModel usuarios)
         {
@@ -42,6 +79,37 @@ namespace ControleDeContatos.Controllers
             catch (Exception ex)
             {
                 TempData["MensagemErro"] = "Ocorreu um erro ao cadastrar um usuário detalhe do erro: " + ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Editar(UsuariosSemSenhaModel usuariosSemSenha)
+        {
+            try
+            {
+                UsuariosModel usuario = null;
+                if (ModelState.IsValid)
+                {
+                    usuario = new UsuariosModel()
+                    {
+                        Id = usuariosSemSenha.Id,
+                        Nome = usuariosSemSenha.Nome,
+                        Email = usuariosSemSenha.Email,
+                        Login = usuariosSemSenha.Login,
+                        Perfil = usuariosSemSenha.Perfil
+                    };
+
+                    usuario =_usuarioRepositorio.Atualizar(usuario);
+                    TempData["MensagemSuccesso"] = "Usuário foi editado com sucesso";
+                    return RedirectToAction("Index");
+                }
+
+                return View(usuario);
+
+            }catch(Exception ex)
+            {
+                TempData["MensagemErro"] = "Ocorreu um erro na edição, erro:" + ex.Message;
                 return RedirectToAction("Index");
             }
         }
--- Models/UsuariosSemSenhaModel.cs
+++ Models/UsuariosSemSenhaModel.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ControleDeContatos.Models
 {
-    public class UsuariosModel
+    public class UsuariosSemSenhaModel
     {
         public int Id { get; set; }
 
@@ -14,15 +14,8 @@ namespace ControleDeContatos.Models
         [Required(ErrorMessage = "Digite o E-mail do usuário")]
         [EmailAddress(ErrorMessage = "O E-mail é inválido")]
         public string Email { get; set; }
-        [Required(ErrorMessage = "Digite a senha do usuário")]
-        public string Senha { get; set; }
         [Required(ErrorMessage = "Selecione o perfil do usuário")]
         public PerfilEnum? Perfil { get; set; }
-        public DateTime DataCadastro { get; set; }
-        public DateTime? AlteracaoUser { get; set; }
-
-        /* Colocando ? após o DateTime, ele passar a não ser um item obrigatório
-         * da criação de um Usuario
-         */
+        
     }
 }
