df98aec30da20d17967daa0cdea1aae0a4454c40
Lucas Simões <lucasbritosimoes@hotmail.com>
2025-08-25 15:52:57 -0300

    Login

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using ControleDeContatos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeContatos.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Entrar(LoginModel loginModel)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    return RedirectToAction("Index","Home"); //Index da ControllerHome
+                }
+
+                return View("Index");
+            }
+            catch (Exception ex)
+            {
+                TempData["MensagemErro"] = "Ops não conseguimos realizar o login, detalhe erro:" + ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+    }
+} 
