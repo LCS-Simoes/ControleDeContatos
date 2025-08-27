aad4db8878f1177f78d0fca976228e8f299cfbda
Lucas Simões <lucasbritosimoes@hotmail.com>
2025-08-27 18:52:14 -0300

    Criptografia de senhas

---
+++ Helper/Criptografia.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Html;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ControleDeContatos.Helper
+{
+    public static class Criptografia
+    {
+        public static string Criptografar(this string valor)
+        {
+            var hash = SHA1.Create();
+            var encode = new ASCIIEncoding();
+            var array = encode.GetBytes(valor);
+
+            array = hash.ComputeHash(array);
+
+            var strHexa = new StringBuilder();
+
+            foreach (var item in array)
+            {
+                strHexa.Append(item.ToString("x2"));
+            }
+
+            return strHexa.ToString();  
+        }
+    }
+}
